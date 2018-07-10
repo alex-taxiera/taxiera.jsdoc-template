001dc447fe6009762c0adb0431ba0944fd8184bf
Alex Taxiera <alex.taxiera@gmail.com>
2018-07-09 22:55:40 -0400

    don't use param attributes inline

--- publish.js
+++ publish.js
@@ -114,10 +114,10 @@ function updateItemName(item) {
         itemName = '&hellip;' + itemName;
     }
 
-    if (attributes && attributes.length) {
-        itemName = util.format( '%s<span class="signature-attributes">%s</span>', itemName,
-            attributes.join(', ') );
-    }
+    // if (attributes && attributes.length) {
+    //     itemName = util.format( '%s<span class="signature-attributes">%s</span>', itemName,
+    //         attributes.join(', ') );
+    // }
 
     return itemName;
 }
